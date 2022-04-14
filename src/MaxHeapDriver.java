711f51841e4629bc6a385f0adc44c60b9fd7b5f9
alan perez <perezalan012400@gmail.com>
2022-04-14 01:34:23 -0700

    Created array of data_random in MaxHeapDriver

--- src/MaxHeapDriver.java
+++ src/MaxHeapDriver.java
@@ -2,16 +2,20 @@ import com.sun.jdi.InterfaceType;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Arrays;
 
 public class MaxHeapDriver
 {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         // Read in input file
         File file = new File("C:\\Users\\perez\\Desktop\\CS 2400\\Project-4\\data_random.txt");
         Scanner sc = new Scanner(file);
 
+        // Test the two methods of creating heaps
         // Initialize array of data points
         Integer[] contents = {1, 2, 3, 4, 5, 6, 7};
 
@@ -27,5 +31,35 @@ public class MaxHeapDriver
 
         System.out.println(heap2.getNumSwaps());
 
+
+        // Perform sequential insertions for the 100 integers
+        // Insert all 100 characters into an array
+        int[] inputContent = new int[100];
+
+        while (sc.hasNextLine())
+        {
+            for (int i = 0; i < inputContent.length; i++)
+            inputContent[i] = Integer.parseInt(sc.nextLine());
+        }
+
+        
+
+
+
+
+        // Write to file
+        try
+        {
+            FileWriter fileWrite = new FileWriter("output.txt");
+
+            while (sc.hasNext())
+            {
+                System.out.println(sc.nextLine());
+            }
+        } catch (IOException e)
+        {
+            System.out.println("An error has occurred. ");
+            e.printStackTrace();
+        }
     }
 }
